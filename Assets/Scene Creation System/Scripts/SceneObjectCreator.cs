a642358293bf493a90bbe9e56b9cb184fd2f122d
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2023-06-03 21:29:55 +0200

    SceneObject easy creation Commit

---
+++ Assets/Scene Creation System/Scripts/SceneObjectCreator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+
+namespace Dhs5.SceneCreation
+{
+    public static class SceneObjectCreator
+    {
+        private static void CreateSceneObject(string path)
+        {
+            GameObject go = (GameObject)AssetDatabase.LoadAssetAtPath(path, typeof(GameObject));
+            GameObject obj = PrefabUtility.InstantiatePrefab(go, Selection.activeTransform) as GameObject;
+            Selection.activeGameObject = obj;
+        }
+
+        [MenuItem("GameObject/SceneObjects/SceneObject", priority = 3)]
+        public static void CreateSimpleSceneObject()
+        {
+            CreateSceneObject("Assets/Scene Creation System/Prefabs/SceneObject.prefab");
+        }
+        
+        [MenuItem("GameObject/SceneObjects/SceneManager", priority = 3)]
+        public static void CreateSceneManager()
+        {
+            CreateSceneObject("Assets/Scene Creation System/Prefabs/SceneManager.prefab");
+        }
+        
+        [MenuItem("GameObject/SceneObjects/SceneClock", priority = 3)]
+        public static void CreateSceneClock()
+        {
+            CreateSceneObject("Assets/Scene Creation System/Prefabs/SceneClock.prefab");
+        }
+    }
+}
+
+#endif
