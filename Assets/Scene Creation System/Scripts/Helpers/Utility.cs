b627e99ea503d3c9c9168275a0f6f2d3203d6dca
Thomas Dhaussy <94963203+DHS5@users.noreply.github.com>
2023-11-29 13:15:39 +0100

    Update Utility.cs

--- Assets/Scene Creation System/Scripts/Helpers/Utility.cs
+++ Assets/Scene Creation System/Scripts/Helpers/Utility.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Dhs5.SceneCreation
 {
     public class Utility : MonoBehaviour
     {
+        #region Editor
+
+#if UNITY_EDITOR
         public static readonly string SceneCreationSettingsPath 
             = "/Settings/Editor/SceneCreationSettings.asset";
         public static readonly string SceneCreationSettingsAutoSavePath 
@@ -17,16 +24,22 @@ namespace Dhs5.SceneCreation
         {
             string settingsContent = File.ReadAllText(Application.dataPath + SceneCreationSettingsPath);
             File.WriteAllText(Application.dataPath + SceneCreationSettingsAutoSavePath, settingsContent);
+            AssetDatabase.Refresh();
         }
         public static void ManualSaveSceneCreationSettings()
         {
             string settingsContent = File.ReadAllText(Application.dataPath + SceneCreationSettingsPath);
             File.WriteAllText(Application.dataPath + SceneCreationSettingsManualSavePath, settingsContent);
+            AssetDatabase.Refresh();
         }
         public static void LoadFromManualSaveSceneCreationSettings()
         {
             string settingsContent = File.ReadAllText(Application.dataPath + SceneCreationSettingsManualSavePath);
             File.WriteAllText(Application.dataPath + SceneCreationSettingsPath, settingsContent);
+            AssetDatabase.Refresh();
         }
+#endif
+
+        #endregion
     }
 }
